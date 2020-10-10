411c6234fe473e322ca088a43999e13046eb9587
Tom Hancocks <tomhancocks@gmail.com>
2020-10-10 05:40:31 +0100

    Missing library header.

--- libGraphite/rsrc/manager.cpp
+++ libGraphite/rsrc/manager.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <algorithm>
 #include <iterator>
 #include "libGraphite/rsrc/manager.hpp"
 #include "libGraphite/rsrc/file.hpp"
