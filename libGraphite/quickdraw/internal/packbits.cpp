6cf89f8df2cdd2688acbdcaf8402ad8ac1b9fed1
Andrew <andrew@digerati.co.nz>
2022-02-19 12:42:09 +1300

    Tidy and improve packbits encode

--- libGraphite/quickdraw/internal/packbits.cpp
+++ libGraphite/quickdraw/internal/packbits.cpp
@@ -4,7 +4,6 @@
 
 #include <stdexcept>
 #include "libGraphite/quickdraw/internal/packbits.hpp"
-#include <stdexcept>
 
 auto graphite::qd::packbits::decode(std::vector<uint8_t> &out_data, const std::vector<uint8_t>& pack_data, std::size_t value_size) -> std::size_t
 {
@@ -44,31 +43,40 @@ auto graphite::qd::packbits::decode(std::vector<uint8_t> &out_data, const std::v
     return out_data.size();
 }
 
-auto graphite::qd::packbits::encode(const std::vector<uint8_t>& scanline_bytes) -> std::vector<uint8_t>
+template
+auto graphite::qd::packbits::encode(const std::vector<uint8_t>& scanline_bytes) -> std::vector<uint8_t>;
+template
+auto graphite::qd::packbits::encode(const std::vector<uint16_t>& scanline_bytes) -> std::vector<uint8_t>;
+template<typename T>
+auto graphite::qd::packbits::encode(const std::vector<T>& scanline_bytes) -> std::vector<uint8_t>
 {
+    unsigned int bits = (sizeof(T) - 1) * 8;
     std::vector<uint8_t> result;
 
     int offset = 0;
     const unsigned long max = scanline_bytes.size() - 1;
-    const unsigned long max_minus_1 = max - 1;
+    // For 8-bit we want to avoid breaking a literal to make a run of 2, as it would generally be less efficient
+    // For 16-bit we should always use runs where possible
+    const unsigned long max_minus_1 = bits ? 0 : max - 1;
 
     while (offset <= max) {
         // Compressed run
         int run = 1;
-        auto replicate = scanline_bytes[offset];
-        while (run < 128 && offset < max && scanline_bytes[offset] == scanline_bytes[offset + 1]) {
+        auto replicate = scanline_bytes[offset++];
+        while (run < 128 && offset <= max && scanline_bytes[offset] == replicate) {
             offset++;
             run++;
         }
 
         if (run > 1) {
-            offset++;
             result.emplace_back(static_cast<uint8_t>(-(run - 1)));
-            result.emplace_back(replicate);
+            for (int b = bits; b >= 0; b -= 8) {
+                result.emplace_back((replicate >> b) & 0xFF);
+            }
+            continue;
         }
 
         // Literal run
-        run = 0;
         while (run < 128 && (offset == max
                             || (offset < max && scanline_bytes[offset] != scanline_bytes[offset + 1])
                             || (offset < max_minus_1 && scanline_bytes[offset] != scanline_bytes[offset + 2]))) {
@@ -76,59 +84,13 @@ auto graphite::qd::packbits::encode(const std::vector<uint8_t>& scanline_bytes) 
             run++;
         }
 
-        if (run > 0) {
-            result.emplace_back(run - 1);
-            for (int i = offset-run; i < offset; ++i) {
-                result.emplace_back(scanline_bytes[i]);
+        result.emplace_back(run - 1);
+        for (int i = offset-run; i < offset; ++i) {
+            for (int b = bits; b >= 0; b -= 8) {
+                result.emplace_back((scanline_bytes[i] >> b) & 0xFF);
             }
         }
     }
 
     return result;
 }
-
-auto graphite::qd::packbits::encode(const std::vector<uint16_t>& scanline_bytes) -> std::vector<uint8_t>
-{
-    std::vector<uint8_t> result;
-
-    int offset = 0;
-    const unsigned long max = scanline_bytes.size() - 1;
-    const unsigned long max_minus_1 = max - 1;
-
-    while (offset <= max) {
-        // Compressed run
-        int run = 1;
-        auto replicate = scanline_bytes[offset];
-        while (run < 128 && offset < max && scanline_bytes[offset] == scanline_bytes[offset + 1]) {
-            offset++;
-            run++;
-        }
-
-        if (run > 1) {
-            offset++;
-            result.emplace_back(static_cast<uint8_t>(-(run - 1)));
-            result.emplace_back(static_cast<uint8_t>(replicate >> 8));
-            result.emplace_back(static_cast<uint8_t>(replicate & 0xFF));
-        }
-
-        // Literal run
-        run = 0;
-        while (run < 128 && (offset == max
-                            || (offset < max && scanline_bytes[offset] != scanline_bytes[offset + 1])
-                            || (offset < max_minus_1 && scanline_bytes[offset] != scanline_bytes[offset + 2]))) {
-            offset++;
-            run++;
-        }
-
-        if (run > 0) {
-            result.emplace_back(run - 1);
-            for (int i = offset-run; i < offset; ++i) {
-                result.emplace_back(static_cast<uint8_t>(scanline_bytes[i] >> 8));
-                result.emplace_back(static_cast<uint8_t>(scanline_bytes[i] & 0xFF));
-            }
-        }
-    }
-
-    return result;
-}
-
