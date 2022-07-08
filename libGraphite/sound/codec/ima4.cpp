8fc603f15b82c6269365030d123d780a75ee1a1c
Tom Hancocks <tomhancocks@gmail.com>
2022-07-08 10:28:40 +0100

    Fix to the IMA4 decoder

--- libGraphite/sound/codec/ima4.cpp
+++ libGraphite/sound/codec/ima4.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <limits>
 #include "libGraphite/sound/codec/ima4.hpp"
 #include "libGraphite/data/writer.hpp"
 
