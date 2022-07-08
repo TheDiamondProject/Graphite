ea0edf507ff82c6bf40d5f4d024a22647a2d6325
Tom Hancocks <tomhancocks@gmail.com>
2022-07-08 19:37:47 +0100

    Begin introducing a custom "rlëX" SpriteWorld resource type

--- libGraphite/spriteworld/rle.hpp
+++ libGraphite/spriteworld/rle.hpp
@@ -27,29 +27,78 @@
 
 namespace graphite::spriteworld
 {
+    template<std::size_t Width>
     struct rle
     {
     public:
-        static auto type_code() -> std::string { return "rlëD"; }
+        static_assert(Width == 16 || Width == 32);
+
+        static auto type_code() -> std::string;
 
     public:
         rle() = default;
-        rle(const quickdraw::size<std::int16_t>& size, std::uint16_t frame_count);
-        explicit rle(const data::block& data, rsrc::resource::identifier id = 0, const std::string& name = "");
-        explicit rle(data::reader& reader);
+
+        rle(const quickdraw::size<std::int16_t>& size, std::uint16_t frame_count)
+            : m_id(0), m_name("RLE"), m_frame_size(size), m_frame_count(frame_count), m_bpp(Width), m_palette_id(0)
+        {
+            _create_surface();
+        }
+
+        explicit rle(const data::block& data, rsrc::resource::identifier id = 0, const std::string& name = "")
+            : m_id(id), m_name(name)
+        {
+            data::reader reader(&data);
+            _decode(reader);
+        }
+
+        explicit rle(data::reader& reader)
+        {
+            _decode(reader);
+        }
 
         ~rle() = default;
 
-        auto surface() -> quickdraw::surface&;
-        [[nodiscard]] auto frames() const -> std::vector<quickdraw::rect<std::int16_t>>;
+        auto surface() -> quickdraw::surface&
+        {
+            return m_surface;
+        }
 
-        [[nodiscard]] auto frame_count() const -> std::size_t;
-        [[nodiscard]] auto frame_rect(std::uint32_t idx) const -> quickdraw::rect<std::int16_t>;
-        [[nodiscard]] auto frame_surface(std::uint32_t idx) const -> quickdraw::surface;
-        auto write_frame(std::uint32_t frame, const quickdraw::surface& surface) -> void;
+        [[nodiscard]] auto frames() const -> std::vector<quickdraw::rect<std::int16_t>>
+        {
+            return {};
+        }
 
-        auto encode(data::writer& writer) -> void;
-        auto data() -> data::block;
+        [[nodiscard]] auto frame_count() const -> std::size_t
+        {
+            return m_frame_count;
+        }
+
+        [[nodiscard]] auto frame_rect(std::uint32_t idx) const -> quickdraw::rect<std::int16_t>
+        {
+            return _frame_rect(idx);
+        }
+
+        [[nodiscard]] auto frame_surface(std::uint32_t idx) const -> quickdraw::surface
+        {
+            return _frame_surface(idx);
+        }
+
+        auto write_frame(std::uint32_t frame, const quickdraw::surface& surface) -> void
+        {
+            _write_frame(frame, surface);
+        }
+
+        auto encode(data::writer& writer) -> void
+        {
+            return _encode(writer);
+        }
+
+        auto data() -> data::block
+        {
+            data::writer writer;
+            _encode(writer);
+            return std::move(*const_cast<data::block *>(writer.data()));
+        }
 
     private:
         enum class pixel_type { type1, type2 };
@@ -73,10 +122,21 @@ namespace graphite::spriteworld
         std::uint16_t m_bpp { 0 };
         std::uint16_t m_palette_id { 0 };
 
-        auto decode(data::reader& reader) -> void;
+        auto _create_surface() -> void;
 
-        [[nodiscard]] auto surface_offset(std::int32_t frame, std::int32_t offset) -> std::uint64_t;
-        auto write_pixel(std::uint16_t pixel, std::uint8_t mask, std::uint64_t offset) -> void;
-        auto write_pixel(std::uint32_t pixel, std::uint8_t mask, std::uint64_t offset, enum pixel_type type) -> void;
+        [[nodiscard]] auto _frame_rect(std::uint32_t idx) const -> quickdraw::rect<std::int16_t>;
+        [[nodiscard]] auto _frame_surface(std::uint32_t idx) const -> quickdraw::surface;
+        auto _write_frame(std::uint32_t frame, const quickdraw::surface& surface) -> void;
+
+        auto _encode(data::writer& writer) -> void;
+        auto _decode(data::reader& reader) -> void;
+
+        [[nodiscard]] auto _surface_offset(std::int32_t frame, std::int32_t offset) -> std::uint64_t;
+
+        auto _write_pixel(std::uint32_t pixel, std::uint8_t mask, std::uint64_t offset) -> void;
+        auto _write_pixel(std::uint64_t pixel, std::uint8_t mask, std::uint64_t offset, enum pixel_type type) -> void;
     };
+
+    template<> auto rle<16>::type_code() -> std::string { return "rlëD"; }
+    template<> auto rle<32>::type_code() -> std::string { return "rlëX"; }
 }
