9dbe6492371d7d5b5039878e67406947cdfcfbba
Tom Hancocks <tomhancocks@gmail.com>
2022-09-25 13:14:43 +0100

    Potential fix to unit tests

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,7 @@
+set(TEST_TARGET_PATH ${CMAKE_SOURCE_DIR}/bin/Graphite_TestRunner -s)
 
 add_test(NAME ConstructDataBlock_Power2Capacity
-        COMMAND GraphiteTestRunner -s ConstructDataBlock_Power2Capacity)
+        COMMAND ${TEST_TARGET_PATH} ConstructDataBlock_Power2Capacity)
 
 add_test(NAME ConstructDataBlock_NonePower2Capacity
-        COMMAND GraphiteTestRunner -s ConstructDataBlock_NonePower2Capacity)
+        COMMAND ${TEST_TARGET_PATH} ConstructDataBlock_NonePower2Capacity)
